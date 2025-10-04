e038e4c8bd077e06116b75608f9b214595883a88
Mubarak Abdul Rahim <mubarakar95@gmail.com>
2025-10-04 11:45:01 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8121617944-e509d",
+  "appId": "1:308921551129:web:60ad537cd814b29a1bdd88",
+  "apiKey": "AIzaSyBvPcyVyuWTEcPhSdUDq33iDD3h-RYxRzo",
+  "authDomain": "studio-8121617944-e509d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "308921551129"
+};
